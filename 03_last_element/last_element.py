0d30502919a7ab69fe7bd6a970ff15c17a93b421
Joshua Lokken <jrrlokken@gmail.com>
2020-06-25 13:09:55 -0500

    Exercise complete

--- 03_last_element/last_element.py
+++ 03_last_element/last_element.py
@@ -1,5 +1,5 @@
 def last_element(lst):
-    """Return last item in list (None if list is empty.
+    """Return last item in list (None if list is empty.)
 
         >>> last_element([1, 2, 3])
         3
--- 25_sum_pairs/sum_pairs.py
+++ 25_sum_pairs/sum_pairs.py
@@ -21,3 +21,14 @@ def sum_pairs(nums, goal):
         >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
         ()
     """
+    seen = set()
+
+    for num in nums:
+        difference = goal - num
+
+        if difference in see:
+            return(difference, num)
+
+        seen.add(num)
+
+    return ()
